2a818a1bc1d36c3db5c653a4a9a4fe8214baf370
seanmilfort <seanmilfort@me.com>
2021-02-12 13:38:36 -0500

    First Commit

---
+++ js/resume.js
@@ -0,0 +1,25 @@
+function responsiveNavbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  
+  window.onscroll = function() {stickyNavbar()};
+
+
+  function stickyNavbar() {
+
+    var navbar = document.getElementById("myTopnav");
+
+    var sticky = navbar.offsetTop;
+
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
