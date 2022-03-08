3ad59cc01f2e572f8b757fed386e98457d86fa7b
Sean Milfort <seanmilfort@me.com>
2022-03-08 14:40:13 -0500

    Created Responsive Navigation Bar

--- js/resume.js
+++ js/resume.js
@@ -1,23 +1,54 @@
-function responsiveNavbar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryHeader = document.querySelector(".primary-header");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (visibility === "false") {
+    primaryNav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
   }
+});
+
+primaryNav.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (visibility === "false") {
+    primaryNav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+});
+
+// function responsiveNavbar() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
 
   window.onscroll = function() {stickyNavbar()};
 
   function stickyNavbar() {
 
     var navbar = document.getElementById("myTopnav");
-    var sticky = navbar.offsetTop;
+    var sticky = primaryHeader.offsetTop;
 
     if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
+      primaryHeader.classList.add("sticky")
     } else {
-      navbar.classList.remove("sticky");
+      primaryHeader.classList.remove("sticky");
     }
     
   };
+
+
+
